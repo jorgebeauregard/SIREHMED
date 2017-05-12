fd2dde4457681933cbc3eb96e508611492f39c22
Julián Huerta <fhuertaymunive@hotmail.com>
2017-05-12 02:44:11 -0500

    veo ninos muertos

--- models/Personal.php
+++ models/Personal.php
@@ -33,7 +33,19 @@ class Personal{
     }
 
     public function update($id,$n_name,$n_last_name,$n_specialty){
-
+        try{
+            $query = $this->mysql->prepare('UPDATE medical_personnel SET 
+                name = ?, last_name = ?, specialty = ?
+            WHERE id = ?');
+            $query->bindParam(1,$n_name, PDO::PARAM_STR);
+            $query->bindParam(2,$n_last_name, PDO::PARAM_STR);
+            $query->bindParam(3,$n_specialty, PDO::PARAM_STR);
+            $query->bindParam(4,$id, PDO::PARAM_INT);
+            $query->execute();
+        }
+        catch(PDOException $e) {
+            echo  $e->getMessage();
+        }
     }
 
     public function delete(){
@@ -54,7 +66,7 @@ class Personal{
     public function getAllPatients(){
         //last name, name, age , gender, weight
         try{
-            $query = $this->mysql->prepare('SELECT name,last_name, age,gender,weight, height FROM patients');
+            $query = $this->mysql->prepare('SELECT id,name,last_name, age,gender,weight, height FROM patients');
             $query->execute();
             return $query->fetchAll(PDO::FETCH_OBJ);
         }
