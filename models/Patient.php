d5baa0369a0c0e875b8e7bf298ff33e333aebdcc
Julián Huerta <fhuertaymunive@hotmail.com>
2017-05-12 00:18:36 -0500

    Show medical procedure method

--- models/Patient.php
+++ models/Patient.php
@@ -120,7 +120,7 @@ class Patient{
     //Show medical procedures //Todo el procedure y nombre completo del doc, specialty
     public function getMedicalProceduresList(){
         try{
-            $query = $this->mysql->prepare('SELECT medical_procedures.cause, medical_procedures.procedure_type, medical_procedures.observations, CONCAT(medical_personnel.name,\' \', medical_personnel.last_name) AS name, medical_personnel.specialty FROM medical_procedures INNER JOIN medical_personnel ON medical_procedures.doctor_id = medical_personnel.id WHERE patient.id = ?');
+            $query = $this->mysql->prepare('SELECT medical_procedures.cause, medical_procedures.procedure_type, medical_procedures.observations, CONCAT(medical_personnel.name,\' \', medical_personnel.last_name) AS name, medical_personnel.specialty FROM medical_procedures INNER JOIN medical_personnel ON medical_procedures.doctor_id = medical_personnel.id WHERE patient_id = ?');
             $query->bindParam(1,$this->id,PDO::PARAM_INT);
             $query->execute();
             return $query->fetchAll(PDO::FETCH_OBJ);
@@ -128,6 +128,19 @@ class Patient{
         }
         catch(PDOException $e) {
             echo  $e->getMessage();
+        }
+    }
+
+    //Show procedure based on id of it
+    public function showMedicalProcedure(){
+        try{
+            $query = $this->mysql->prepare('SELECT medical_procedures.cause, medical_procedures.procedure_type, medical_procedures.observations,medical_procedures.date_realized, CONCAT(medical_personnel.name,\' \', medical_personnel.last_name) AS name, medical.medical_personnel.specialty FROM medical_procedures INNER JOIN medical_personnel ON medical_procedures.doctor_id = medical_personnel.id WHERE medical_procedures.id = ?');
+            $query->bindParam(1,$this->id,PDO::PARAM_INT);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_OBJ);
+        }
+        catch(PDOException $e){
+
         }
     }
 
