efe13720aadc181557b174e21d0bcda9443f7e90
Julián Huerta <fhuertaymunive@hotmail.com>
2017-05-11 20:03:39 -0500

    Added db things

---
+++ models/Patient.php
@@ -0,0 +1,21 @@
+<?php
+class Patient{
+	private $psql;
+    private $mysql;
+
+    private $id;
+    private $name;
+    private $last_name;
+    private $age;
+    private $gender;
+    private $blood_type;
+    private $birthdate;
+    private $emergency_name;
+    private $emergency_phone;
+    private $body_mass_index;
+    private $active;
+
+
+?>
+
+
